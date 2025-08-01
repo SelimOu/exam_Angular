ca6d07f7cd46e259bf1c5ac6873e65de2930559d
Selim <ounoughi.selim@gmail.com>
2025-08-01 13:34:21 +0200

    modification de la page d'acceuil

--- Exam_Angular/src/app/composants/accueil/accueil.component.ts
+++ Exam_Angular/src/app/composants/accueil/accueil.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ArticleService } from '../../services/article.service';
+import { Article } from '../../models/article.interface';
 
 @Component({
   selector: 'app-accueil',
@@ -9,4 +11,24 @@ import { RouterModule } from '@angular/router';
   templateUrl: './accueil.component.html',
   styleUrl: './accueil.component.css'
 })
-export class AccueilComponent { }
+export class AccueilComponent implements OnInit {
+  articlesEnPromo: Article[] = [];
+  loading = true;
+
+  constructor(private articleService: ArticleService) { }
+
+  ngOnInit() {
+    this.articleService.getAll().subscribe(articles => {
+      this.articlesEnPromo = articles.filter(article => article.discountPercent > 0);
+      this.loading = false;
+    });
+  }
+
+  getPrixPromo(article: Article): number {
+    return article.fullPrice * (1 - article.discountPercent);
+  }
+
+  getPourcentagePromo(discountPercent: number): number {
+    return Math.round(discountPercent * 100);
+  }
+}
